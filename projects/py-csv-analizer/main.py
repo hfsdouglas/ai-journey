ed92555c2d097a285facc43eeaaa5124907567e3
Douglas Faria <hfsdouglas@gmail.com>
2026-06-02 19:39:31 -0300

    fix: atualiza estrutura para classe

--- projects/py-csv-analizer/main.py
+++ projects/py-csv-analizer/main.py
@@ -1,49 +1,59 @@
 import os
 
-from analizer import Analizer
+class Analisador:
+    def __init__(self):
+        self.caminho = ""
+        
+        self.iniciar()
 
-def limpa_terminal():
-    """Limpa o terminal de acordo com o sistema operacional."""
-    os.system('cls' if os.name == 'nt' else 'clear')
+    def limpa_terminal(self):
+        """Limpa o terminal de acordo com o sistema operacional."""
+        os.system('cls' if os.name == 'nt' else 'clear')
 
-print(f"================================\n"
-       " Bem Vindo ao Analisador de CSV \n"
-       "================================\n")
+    def iniciar(self):
+        """Inicializa o sistema de análise de dados."""
+        print(f"==================================\n"
+               " Analise de dados do INEP por CSV \n"
+               "==================================\n")
 
-arquivo = input(f"Digite o nome do arquivo CSV: ")
+        arquivo = input(f"Digite o nome do arquivo CSV: ")
 
-try: 
-    caminho = f"data/{arquivo}.csv";
+        try: 
+            self.caminho = f"data/{arquivo}.csv";
 
-    if not os.path.exists(caminho):
-        raise FileNotFoundError()
+            if not os.path.exists(self.caminho):
+                raise FileNotFoundError()
 
-    print(f"\nArquivo '{arquivo}' selecionado!\n")
+            print(f"\nArquivo '{arquivo}' selecionado!\n")
 
-    print("[1] - Analisar o arquivo")
-    print("[2] - Visualizar dashboard")
-    print("[3] - Executar tudo")
-    print("[4] - Limpar dados")
-    print("[5] - Sair\n")
+            print("[1] - Analisar o arquivo")
+            print("[2] - Visualizar dashboard")
+            print("[3] - Executar tudo")
+            print("[4] - Limpar dados")
+            print("[5] - Sair\n")
 
-    opcao = input("O que deseja fazer? ")
+            opcao = input("O que deseja fazer? ")
 
-    match opcao: 
-        case "1":
-            limpa_terminal()
+            match opcao: 
+                case "1":
+                    self.limpa_terminal()
+                    self.analisar_arquivo()
+                case "2":
+                    print("Limpar dados selecionado!")
+                case "3":
+                    print("Visualizar dashboard selecionado!")
+                case "4":
+                    print("Executar tudo selecionado!")
+                case "5":
+                    print("Saindo do programa...")
+                case _:
+                    print("Opção inválida. Por favor, escolha uma opção válida.")
 
-            analizer = Analizer(caminho)
-            analizer.start()
-        case "2":
-            print("Limpar dados selecionado!")
-        case "3":
-            print("Visualizar dashboard selecionado!")
-        case "4":
-            print("Executar tudo selecionado!")
-        case "5":
-            print("Saindo do programa...")
-        case _:
-            print("Opção inválida. Por favor, escolha uma opção válida.")
+        except FileNotFoundError:
+            print(f"\nArquivo '{arquivo}' não encontrado. Verifique o nome e tente novamente.\n")
 
-except FileNotFoundError:
-    print(f"\nArquivo '{arquivo}' não encontrado. Verifique o nome e tente novamente.\n")
+    def analisar_arquivo(self):
+        """Função para analisar o arquivo CSV."""
+        print("Analisando o arquivo...")
+
+Analisador()
